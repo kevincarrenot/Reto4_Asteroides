38fc883569600066bc6b33df5118e7fd9b814ccf
Kevin Carreño <kevincarrenot@gmail.com>
2020-10-21 00:22:39 -0500

    Actualizacion

---
+++ Assets/Scripts/Asteroides.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Asteroides : MonoBehaviour
+{
+    public float speed = 8;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.back * speed+ Vector3.right*Random.Range(-1,1);
+        rb.angularVelocity = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float speed = 20f;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Asteroide"))
+        {
+            Destroy(gameObject);
+            other.SendMessage("Colision", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+    void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
