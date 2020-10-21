3149559c49cb6c1db90ba837a6fc0cd0301bd571
Kevin Carreño <kevincarrenot@gmail.com>
2020-10-21 00:16:17 -0500

    Commit Juego

---
+++ RetoAsteorides/Assets/Scripts/Disparo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public Transform lanzadorIzq;
+    public Transform lanzadorDer;
+
+    public GameObject DisparoPrefab;
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(DisparoPrefab, lanzadorIzq.position, DisparoPrefab.transform.rotation);
+            Instantiate(DisparoPrefab, lanzadorDer.position, DisparoPrefab.transform.rotation);
+
+        }
+    }
+}
---
+++ RetoAsteorides/Assets/Scripts/NaveMovimiento.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveMovimiento : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 8f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Direccionx = Input.GetAxis("Horizontal") * Vector3.right;
+        Vector3 Direccionz = Input.GetAxis("Vertical") * Vector3.forward;
+
+        Vector3 Direccion = Direccionx + Direccionz;
+        Vector3 VectorVelocidad = Direccion * speed;
+        rb.velocity = VectorVelocidad;
+    }
+}
